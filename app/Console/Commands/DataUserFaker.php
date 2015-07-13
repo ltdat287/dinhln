e579a82f48e00a3fb160e0ab3c5232cc82dfd602
datlt <ltdat5789@gmail.com>
2015-07-13 14:27:55 +0700

    add DataUserFaker to create data for user

--- app/Console/Commands/DataUserFaker.php
+++ app/Console/Commands/DataUserFaker.php
@@ -3,7 +3,7 @@
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
-use VirtualProject\User;
+use VirtualProject\User as Users;
 use Faker\Factory as Faker;
 use Validator;
 
@@ -42,45 +42,47 @@ class DataUserFaker extends Command {
 	{
 		//
 		$boss_id = $this->argument('boss_id');
-        $create = $this->option('create');
+        $limit = $this->option('limit');
 
-        //Validate input values of boss_id
+        // Validate input values of boss_id
         $valids = Validator::make(array(
         	'boss_id' => $boss_id,
-        	'create' => $create
+        	'limit' => $limit
         ), array(
         	'boss_id' => 'required|numeric',
-        	'create' => 'required|numeric'
+        	'limit' => 'required|numeric'
         ));
 
-        if ($valids->fails()){
+        if ($valids->fails()) {
         	foreach($valids->messages()->all() as $msg){
         		$this->error($msg);
         	}
 
         	return $this->error('You need set values.');
-        } else{
-        	if ($this->confirm('Do you want add user used Faker? [y/n]'))
-        	{
+        } else {
+        	if ($this->confirm('Do you want add user used Faker? [y/n]')) {
+        		// Insert data faker for users.
         		$faker = Faker::create();
-     			for ($i = 0; $i < $create; $i++){
-                $user = new Users;
-                $user->name        = $faker->name;
-                $user->email       = $faker->email;
-                $user->password    = $faker->password;
-                $user->kana 	   = $faker->name;
-                $user->telephone_no= $faker->phoneNumber;
-                $user->birthday	   = $faker->date($format = 'Y-m-d', $max = 'now');
-                $user->note        = $faker->text;
-                $user->boss_id     = $boss_id;
-                $user->updated_at   = \Carbon::create(2015, 1, 4, 11, 12, 13);
-            	$user->created_at   = \Carbon::create(2015, 1, 4, 11, 12, 13);
-                $user->save();
+     			for ($i = 0; $i < $limit; $i++) {
+	                $user = new Users;
+	                $user->name         = $faker->name;
+	                $user->email        = $faker->email;
+	                $user->password     = $faker->password;
+	                $user->kana 	    = $faker->name;
+	                $user->telephone_no = $faker->phoneNumber;
+	                $user->birthday	    = $faker->date($format = 'Y-m-d', $max = 'now');
+	                $user->note         = $faker->text;
+	                $user->boss_id      = $boss_id;
+	                $user->updated_at   = $faker->dateTime($max = 'now');
+	            	$user->created_at   = $faker->dateTime($max = $user->updated_at);
+	                $user->save();
 
-                $this->info('Inserted ' . $i . ' records for user to database with boss_id:'.$boss_id);
+	                $this->info('Inserted ' . $i . ' records for user to database with boss_id:' . $boss_id);
             	}
-	            $headers = ['id','name','email','phone'];
-	            $content = Users::all(['id', 'name','email','phone'])->toArray();
+
+				// Draw table into console.
+	            $headers = ['id', 'name', 'email', 'password', 'boss_id'];
+	            $content = Users::all(['id', 'name', 'email', 'password', 'boss_id'])->toArray();
 	            $this->table($headers, $content);
 
 	            return $this->info('Inserted database successful!');   		
@@ -110,7 +112,7 @@ class DataUserFaker extends Command {
 	protected function getOptions()
 	{
 		return [
-			['create', null, InputOption::VALUE_OPTIONAL, 'Amount of user if you want add.', null],
+			['limit', null, InputOption::VALUE_OPTIONAL, 'Amount of user if you want add.', null],
 		];
 	}
 
