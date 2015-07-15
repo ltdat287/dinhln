bdf6b8a6d6501582c2fff57280be9bc5f1346078
datlt <ltdat5789@gmail.com>
2015-07-15 17:56:14 +0700

    test code javascript

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,9 +26,49 @@
 
 <script>
 	$(document).ready(function(){
-		$(".checkbox").click(function(){
-			alert($("#check").val());
+		
+		// if ($.cookie('valueCheck')) {
+		// 		arrayCheck.concat($.cookie('valueCheck'));
+		// 	}
+		// 	
+		
+		// Get checked ids from cookie
+		var arrayCheck = $.cookie('cookieCheck');
+
+		// Set checked for checkboxes exists ids in cookie.
+		if (typeof(arrayCheck) == 'undefined') {
+			var arrayCheck = [];
+		}
+		$(".check").each(function () {
+			var checkedVal = $(this).val();
+			if (arrayCheck.indexOf(checkedVal) >= 0) {
+				$(this).attr('checked', 'checked');
+			}
 		});
+
+		// Bind event for checked change checboxes.
+		$(".check").click(function(){
+			console.log(arrayCheck);
+			$('.check').each(function() {
+				if ($(this).is(':checked')) {
+					arrayCheck.push($(this).val());
+				}
+			});
+			$.cookie('cookieCheck',arrayCheck);
+			
+			//var arrayCheck = [];
+			//$('.check:checked').each(function() {
+				//var currentVal = $(this).val();
+				//if (arrayCheck.indexOf(currentVal) < 0) {
+					//arrayCheck.push(currentVal);
+				//}
+				
+			//});
+			// $.cookie('valueCheck',arrayCheck);
+		});
+		// $('a').click(function() {
+			
+		// });
 	});
 </script>
 </footer>
